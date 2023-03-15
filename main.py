9e3f43582723ba02480ebf1c410144ea98cb7474
kaustubh-s1 <shuklakaustubh84@gmail.com>
2023-03-15 16:52:13 +0530

    feat(contribution & docs): 🏁 add contribution guide and code comments

--- main.py
+++ main.py
@@ -7,15 +7,20 @@ from PyQt5.QtGui import QPixmap
 from PyQt5.QtWidgets import (QApplication, QPushButton)
 from PyQt5.QtWidgets import QMainWindow
 
+# Define a class that inherits from QMainWindow and QPushButton
 class MainWindow(QMainWindow, QPushButton):
+    # Constructor to initialize the class
 	def __init__(self, widget):
 		super(MainWindow, self).__init__()
-		self.widget = widget
+		self.widget = widget # Initialize the widget
   
+		# Load the user interface from a .ui file
 		loadUi('ui/home.ui', self)
+		# Load an image file and set it as the pixmap for an image label widget
 		pic = QPixmap('static/uoa_logo.png')
 		self.imglabel.setPixmap(pic)
 
+		# Find and initialize three push buttons, and connect them to their respective methods
 		self.visualiseB = self.findChild(
 			QtWidgets.QPushButton, 'visualise_button')
 		self.visualiseB.clicked.connect(self.goto_visualize)
@@ -28,19 +33,34 @@ class MainWindow(QMainWindow, QPushButton):
 			QtWidgets.QPushButton, 'liveAudio_button')
 		self.liveAudioB.clicked.connect(self.goto_liveAudio)
 
+	# Method that is called when the "visualise" button is clicked
 	def goto_visualize(self):
+		"""
+		It creates an instance of the visualisationScreen class, adds it to the widget stack, and then sets
+		the current widget to the visualisationScreen
+		"""
 		from components.visualize import visualisationScreen
 		visualise = visualisationScreen(self.widget)
 		self.widget.addWidget(visualise)
 		self.widget.setCurrentIndex(self.widget.currentIndex() + 1)
 
+	# Method that is called when the "annotate" button is clicked
 	def goto_annotate(self):
+		"""
+		It creates an instance of the annotationScreen class, adds it to the widget stack, and then sets the
+		current widget to the annotationScreen
+		"""
 		from components.annotation import annotationScreen
 		annotate = annotationScreen(self.widget)
 		self.widget.addWidget(annotate)
 		self.widget.setCurrentIndex(self.widget.currentIndex() + 1)
 
+	# Method that is called when the "live audio" button is clicked
 	def goto_liveAudio(self):
+		"""
+		It creates a new instance of the liveAudioScreen class, adds it to the widget stack, and then sets
+		the current widget to the new instance
+		"""
 		from components.live_audio import liveAudioScreen
 		liveAudio = liveAudioScreen(self.widget)
 		self.widget.addWidget(liveAudio)
